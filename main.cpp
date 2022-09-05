2e67fa3fb1ea27bfed20f5cada1fe7c6c7be7e07
Fadhilah Aditya Akbar <111202214130@mhs.dinus.ac.id>
2022-09-05 18:41:42 +0700

    Tugas1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Fadhilah Aditya Akbar" << endl;
+    cout << "A11.2022.14130" << endl;
+    cout << "Tompogunung Rt04/Rw10, Klongan, Kec Umgaran Timur, Kab Semarang" << endl;
+    cout << "SMK N 11 SEMARANG" << endl;
+    return 0;
+}
